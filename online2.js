d9f287f678242f330b3c557b28781b90ded7af08
RAFAEL FERREIRA DE SIQUEIRA <89710245+RafaelFerreira7@users.noreply.github.com>
2021-09-16 23:11:47 -0300

    Add files via upload

---
+++ online2.js
@@ -0,0 +1,34 @@
+//data de inicio do cadastro para o evento
+
+var datainicio = 15
+
+
+if(datainicio >= 15 ){
+    console.log("cadastro permitido")
+
+}else{
+  
+    console.log ("cadstro não permitido por data invalida   / FIm")
+}
+
+//idade do palestrante ou participante
+
+var idade =18
+
+    if(idade >= 18 ){
+        console.log("permitido o cadastro")
+
+    }else{
+        console.log("cadastro não permitido pela idade     /fim")
+    }
+
+//paticipantes do evento
+
+var participantes =100
+
+    if(participantes <= 100 ){
+        console.log("cadastro permitido     /fim")
+
+    }else{
+        console.log("cadastro não permitido por ter excedido o limite  /fim")
+} 
